4feb54cb01a9053fac5689a610c639d2d8cf15be
柏超曾 <565618435@qq.com>
2017-08-23 13:22:28 +0800

    initialise

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by 柏超曾 on 2017/8/23.
+//  Copyright © 2017年 柏超曾. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by 柏超曾 on 2017/8/23.
+//  Copyright © 2017年 柏超曾. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"123");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
